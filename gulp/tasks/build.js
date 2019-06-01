46da77160addb94baaf125d440da7c4b5433ca2a
Steve <steverehm@online.de>
2019-06-02 00:24:20 +0200

    ready to rock

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -45,7 +45,7 @@ gulp.task('optimizeImages', ['deleteDistFolder'], function () {
 });
 
 gulp.task('useminTrigger', ['deleteDistFolder'], function(){
-    gulp.setMaxListeners('usemin');
+    gulp.start('usemin');
 });
 
 gulp.task('usemin', ['styles', 'scripts'], function () {
