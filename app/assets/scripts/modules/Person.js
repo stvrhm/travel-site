0e95cbdae930f4d25bb3266c134dd84ee4dd0c51
Steve <steverehm@online.de>
2019-06-01 14:08:20 +0200

    mobile navigation completed

--- app/assets/scripts/modules/Person.js
+++
@@ -1,9 +0,0 @@
-function Person(name, color) {
-    this.name = name;
-    this.color = color;
-    this.greet = function() {
-        console.log("Hello, my name is " + this.name + " and I like the color " + this.color + " .");
-    }
-}
-
-module.exports = Person;
